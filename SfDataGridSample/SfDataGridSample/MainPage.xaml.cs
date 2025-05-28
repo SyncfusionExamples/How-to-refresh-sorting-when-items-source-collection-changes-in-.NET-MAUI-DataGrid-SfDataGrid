ddc1bfbbc608b9b0d1f9f3ddea9dca2441555035
Abinaya-SF4661 <abinaya.srinivasan@syncfusion.com>
2025-05-28 18:09:21 +0530

    Added sample for sorting

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.OrderInfoCollection.Clear();
+            ViewModel.ChangeCollection();
+        }
+    }
+
+}
